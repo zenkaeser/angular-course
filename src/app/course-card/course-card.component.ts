82fe8580fec4494572d58d7c3041564b478d1d07
zenkaeser <kss@digizuite.com>
2021-04-09 23:28:00 +0200

    Angular View Encapsulation - Default vs Shadow DOM

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,23 +1,28 @@
-import {
-    AfterContentInit,
-    AfterViewInit,
-    Component,
-    ContentChildren,
-    ElementRef,
-    EventEmitter,
-    Input,
-    OnInit,
-    Output,
-    QueryList,
-    ViewEncapsulation
-} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {Course} from '../model/course';
-import {CourseImageComponent} from '../course-image/course-image.component';
 
 @Component({
     selector: 'course-card',
     templateUrl: './course-card.component.html',
-    styleUrls: ['./course-card.component.css']
+    styleUrls: ['./course-card.component.css'],
+    encapsulation:
+      // ViewEncapsulation.Emulated,
+        // default, uses special attributes added on the element, recommended
+
+      // ViewEncapsulation.None
+        // if you want to style your application with plain CSS, CSS modifier will not be used.
+        // all styles will be bundled together into a one global stylesheet w/o further processing
+
+      ViewEncapsulation.ShadowDom
+        // built in browser mechanism for doing view encapsulation.
+        // It does something similar to the emulated option that allows us to built styles that are specific to only
+        // certain elements of the page
+        // but, it does so natively directly using browser provided functionality. Instead of using special attributes for increasing
+        // the specificity of the components styles.
+        // all html of the host element is inside the 'shadow-root' - it is like a new browser document very similar to the page where we
+        // are adding this component. And, inside it is a series of styles. The styles that are getting applied to our component
+        // and they are only visible inside the shadow-root. These styles are not going to affect the rest of the page.
+        // By definition the styles are confined in the interior of shadow-root which is a separate html document.
 })
 export class CourseCardComponent implements OnInit {
 
